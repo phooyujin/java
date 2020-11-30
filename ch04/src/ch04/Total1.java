0ad22c298660a62407dda26ba07f7c632cbe1c4f
phooyujin <phoo0517@gmail.com>
2020-11-30 21:13:18 +0900

    arr ex

--- ch04/src/ch04/Total1.java
+++ ch04/src/ch04/Total1.java
@@ -2,6 +2,14 @@ package ch04;
 
 public class Total1 {
 	public static void main(String[] args) {
-		
+		//합계와 평균을 구하세요
+		int [] a = {85,90,75,100,95};
+		int sum=0;
+		//일반 for
+		for(int i=0; i<a.length; i++) {
+			sum +=a[i];
+		}
+		System.out.println("합계: "+sum);
+		System.out.println("평균: "+sum/a.length);
 	}
 }
